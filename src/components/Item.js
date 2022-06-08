e9db74a2ee59da2754112768f886e0a7e0a6ece8
Lyn Finman <lynfinman@gmail.com>
2022-06-08 10:57:11 -0400

    Updated item to add/remove from shopping cart

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+  const itemClass = inCart ? "in-cart" : ""
+  const cartButtonText = inCart ? "Remove from Cart" : "Add to Cart";
+
+  function handleCartRequest() {  
+    setInCart(!inCart);
+  }
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleCartRequest} className="add">{cartButtonText}</button>
     </li>
   );
 }
